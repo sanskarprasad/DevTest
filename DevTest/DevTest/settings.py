ae53105e778c5ce8f9d764f94376e8e7b6c663b0
sanskarprasad <psanskarkumar01@gmail.com>
2024-08-14 00:20:41 +0530

    just before deployment

--- DevTest/DevTest/settings.py
+++ DevTest/DevTest/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%0s$7&ca_4b5jhzxdscy61r8p$wue_u$kc63@5@j0+7%yqifj='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['sanskar01.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 
